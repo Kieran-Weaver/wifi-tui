e9aa6f6d41710df20e5272e469a81475a79d2082
Kieran Weaver <drawbook.productions@gmail.com>
2023-12-31 18:31:46 -0800

    Receive messages successfully

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-intf:
+intf: intf.c
 	gcc -O0 -g -I/usr/include/libnl3/ -o intf intf.c -lnl-3
 
 all: intf
--- intf.c
+++ intf.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdatomic.h>
+#include <string.h>
+
+#define MAX_INTFS 256
 
 struct intf {
 	char* name;
@@ -12,18 +15,46 @@ struct intf {
 };
 
 struct intfs {
-	volatile int done; // callback finished?
 	int len; // number of interfaces
 	struct intf *intfs;
 };
 
-static int intf_cb( struct nl_msg *msg, void* ctx ) {
-	struct intfs *intfs = (struct intfs*) ctx;
-	if ( intfs->done ) {
-		return NL_OK;
+static void add_intf( struct intfs* intfs, int i, const char* name ) {
+	int len = strlen( name );
+
+	intfs->intfs[ i ].len = len;
+	intfs->intfs[ i ].name = malloc( len + 1 );
+	memcpy( intfs->intfs[ i ].name, name, len );
+	intfs->intfs[ i ].name[ len ] = '\0';
+}
+
+static void free_intfs( struct intfs* intfs ) {
+	int i;
+
+	for ( i = 0; i < intfs->len; i++ ) {
+		free( intfs->intfs[ i ].name );
 	}
 
-	atomic_store( &intfs->done, true );
+	free( intfs );
+}
+
+static int intf_cb( struct nl_msg *msg, void* ctx ) {
+	struct intfs *intfs = (struct intfs*) ctx;
+
+	struct nlmsghdr *nl_hdr = nlmsg_hdr( msg );
+	struct ifinfomsg *intf = NLMSG_DATA( nl_hdr );
+	struct rtattr *rta = IFLA_RTA( intf );
+	int bytes_left = nl_hdr->nlmsg_len - NLMSG_LENGTH(sizeof(*intf));
+
+	while ( RTA_OK( rta, bytes_left ) ) {
+		if ( rta->rta_type == IFLA_IFNAME ) {
+			const char* name = RTA_DATA( rta );
+			add_intf( intfs, intfs->len, name );
+			intfs->len++;
+		}
+
+		rta = RTA_NEXT( rta, bytes_left );
+	}
 
 	return NL_OK;
 }
@@ -31,6 +62,7 @@ static int intf_cb( struct nl_msg *msg, void* ctx ) {
 int main() {
 	struct nl_sock *sock = nl_socket_alloc();
 	struct intfs *intfs = calloc( 1, sizeof( struct intfs ) );
+	intfs->intfs = calloc( MAX_INTFS, sizeof( struct intf ) );
 
 	nl_connect( sock, NETLINK_ROUTE );
 
@@ -42,9 +74,12 @@ int main() {
 
 	nl_socket_modify_cb( sock, NL_CB_VALID, NL_CB_CUSTOM, intf_cb, intfs );
 	nl_recvmsgs_default( sock );
-	assert( intfs->done );
 
-	free( intfs );
+	for ( int i = 0; i < intfs->len; i++ ) {
+		printf( "%s\n", intfs->intfs[ i ].name );
+	}
+
+	free_intfs( intfs );
 	nl_socket_free( sock );
 
 	return 0;
